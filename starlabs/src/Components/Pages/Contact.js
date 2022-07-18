b114cd571d9dbff24fddd8436804ba45a3672799
Arndrit <95188989+arndritb@users.noreply.github.com>
2022-07-18 19:38:46 +0200

    finished Contact from and made it responsive also added some social icons

--- starlabs/src/Components/Pages/Contact.js
+++ starlabs/src/Components/Pages/Contact.js
@@ -1,6 +1,8 @@
 import React from "react";
 import '../Pages/Contact.css'
 import emailjs from 'emailjs-com'
+import {Github, Twitter, Facebook } from '../Assets/AllSvgs';
+import { Link} from 'react-router-dom';
 
 export const Contact = () => {
 
@@ -20,23 +22,86 @@ export const Contact = () => {
         <div className="cont">
             <div className="ainer">
                 <div>
-                    <h1>Contact Form</h1>
-                    <form className="forma" onSubmit={sendEmail}>
-                        <label>Name</label>
-                        <input type='text' name='name' />
+                    <h1 className="section-header">Contact Me</h1>
 
-                        <label>Email</label>
-                        <input type='email' name='user_email' />
+                    {/* left contact form */}
+                    <div className="contact-wrapper">
+                        <form className="form-horizontal" onSubmit={sendEmail}>
+                            <div className="form-group">
+                                <div className="col-sm-12">
+                                    <input 
+                                        className="form-control"
+                                        type='text'
+                                        name='name' 
+                                        placeholder="NAME"
+                                        required/>
+                                </div>
+                            </div>
 
-                        <label>Message</label>
-                        <textarea name='message' rows='4' />
+                            <div className="form-group">
+                                <div className="col-sm-12">
+                                    <input 
+                                        className="form-control"
+                                        type='email' 
+                                        name='user_email'
+                                        placeholder="EMAIL"
+                                        required />
+                                </div>
+                            </div>
+                            
+                            <textarea 
+                                className="form-controla"
+                                name="message"
+                                rows="10"
+                                placeholder="MESSAGE"
+                                required
+                            />
+                       
 
-                        <input type='submit' value='Send' />
-                        
-                    </form>
+                            <input 
+                                className="btn btn-primary send-button"
+                                type='submit' 
+                                value='SEND' />
+                            
+                        </form>
+
+
+                        {/* right contact form */}
+                        <div class="direct-contact-container">
+
+                            <ul class="contact-list">
+                                <li className="list-item"><span className="contact-text place">Prishtine, Kosove</span></li>
+                                <li className="list-item"><span className="contact-text phone"><a href="tel:049796839" title="Give me a call">049-796-839</a></span></li>
+                                <li className="list-item"><span className="contact-text gmail"><a href="mailto:#" title="Send me an email">arndritb@gmail.com</a></span></li>
+                            </ul>
+
+                                <div className="icons">
+                                    <Link 
+                                        // style={{color:'inherit'}} 
+                                        target="_blank" 
+                                        to={{pathname:"https://github.com/arndritb"}} >
+                                        <Github width={25} height={25} />
+                                        
+                                    </Link>
+
+                                    <Link 
+                                        // style={{color:'inherit'}} 
+                                        target="_blank" 
+                                        to={{pathname:"https://twitter.com/arndritbaj"}}>
+                                        <Twitter width={25} height={25} />
+                                    </Link>
+
+                                    <Link 
+                                        // style={{color:'inherit'}} 
+                                        target="_blank" 
+                                        to={{pathname:"https://www.facebook.com/arndrit.b/"}}>
+                                        <Facebook width={25} height={25} />
+                                    </Link>
+                                </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-            
         </div>
     )
 }
